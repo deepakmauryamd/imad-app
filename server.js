927dfd7004668db6c2cff880b5fb5224ae0af7d9
deepakmauryamd <30929093+deepakmauryamd@users.noreply.github.com>
2017-08-14 15:54:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/md.html', function (req, res) {
+app.get('/md', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'md.html'));
 });
 
