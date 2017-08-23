a12bd953eb97c44424ada5a15ea1229eb45e2a13
deepakmauryamd <30929093+deepakmauryamd@users.noreply.github.com>
2017-08-23 18:43:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/Head-first lounge', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Head-first lounge.html'));
+app.get('/Head-first', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Head-first.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
